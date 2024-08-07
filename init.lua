a8952847db8226dde00ccfc308a4a9e3788792a3
Kyle Cain <kylecain0305@gmail.com>
2024-08-06 23:04:34 -0400

    remove whichkey

--- init.lua
+++ init.lua
@@ -44,22 +44,4 @@ require("lazy").setup({
         event = "InsertEnter",
         opts = {}
     },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
-            },
-        },
-    },
 }, opts)
