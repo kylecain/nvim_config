9a8012097297858c91343f537f13f3c1a6e22be0
Kyle Cain <kylecain0305@gmail.com>
2025-11-12 20:21:47 -0500

    add autopairs

--- init.lua
+++ init.lua
@@ -373,6 +373,11 @@ require("lazy").setup({
 			indent = { enable = true, disable = { "ruby" } },
 		},
 	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
 })
 
 -- vim: ts=2 sts=2 sw=2 et
