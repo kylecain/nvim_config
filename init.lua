de1197891600d57bbf9c058f0a2f4352c065a988
Kyle Cain <kylecain0305@gmail.com>
2025-11-12 20:44:41 -0500

    add remap for netrw

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ vim.opt.scrolloff = 10
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open [E]xplore" })
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
